ddc9fe6749e863d83d17a3272d0af8ad52c5ec48
Mitsu325 <patricia.mashiba@gmail.com>
2022-02-13 20:38:58 -0300

    include libft

---
+++ libft/includes/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: patricia <patricia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/07 21:33:53 by patricia          #+#    #+#             */
+/*   Updated: 2021/06/19 20:06:25 by patricia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+void	ft_putchar_fd(char ch, int fd);
+void	ft_putstr_fd(char *str, int fd);
+
+#endif
