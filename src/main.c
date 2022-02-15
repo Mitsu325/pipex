06d17b75c89e501b39906e098a3f311b71c2ed86
Mitsu325 <patricia.mashiba@gmail.com>
2022-02-15 02:55:07 +0100

    Change return find env path function

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 22:07:37 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/02/14 01:31:26 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/02/15 02:54:04 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	find_env_path(char **envp)
+char	**find_env_path(char **envp)
 {
 	char	**env_path;
 	int		i;
@@ -21,20 +21,21 @@ void	find_env_path(char **envp)
 	while (ft_strnstr(envp[i], "PATH", 4) == NULL)
 		i++;
 	env_path = ft_split(envp[i] + 5, ':');
-	ft_putstr_fd(env_path[0], STDOUT);
-	ft_putstr_fd("\n", STDOUT);
+	return (env_path);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	// int		pid;
 	// int		fd[2];
-	// char	**env_path;
+	char	**env_path; // ! norminette: Variable declaration not at start of function
 
-	ft_putstr_fd(argv[0], STDOUT);
-	ft_putstr_fd("\n", STDOUT);
 	if (argc != 5)
 		error_handler("Bad arguments", "Ex: ./pipex file1 cmd1 cmd2 file2");
-	find_env_path(envp);
+	env_path = find_env_path(envp);
+	ft_putstr_fd(env_path[1], STDOUT);
+	ft_putstr_fd("\n", STDOUT);
+	ft_putstr_fd(argv[0], STDOUT);
+	ft_putstr_fd("\n", STDOUT);
 	return (EXIT_SUCCESS);
 }
