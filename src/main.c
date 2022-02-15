e5ccfed2c01a22d8e563d27a4585942be88921bf
Mitsu325 <patricia.mashiba@gmail.com>
2022-02-16 00:54:33 +0100

    refactoring >> find_env_path

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 22:07:37 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/02/15 02:54:04 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/02/16 00:51:38 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,19 @@
 
 char	**find_env_path(char **envp)
 {
-	char	**env_path;
-	int		i;
+	int	i;
 
 	i = 0;
 	while (ft_strnstr(envp[i], "PATH", 4) == NULL)
 		i++;
-	env_path = ft_split(envp[i] + 5, ':');
-	return (env_path);
+	return (ft_split(envp[i] + 5, ':'));
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	// int		pid;
 	// int		fd[2];
-	char	**env_path; // ! norminette: Variable declaration not at start of function
+	char	**env_path;
 
 	if (argc != 5)
 		error_handler("Bad arguments", "Ex: ./pipex file1 cmd1 cmd2 file2");
