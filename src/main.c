9bc3259d161f30c39fd7d2854b5875785f40dfa0
Mitsu325 <patricia.mashiba@gmail.com>
2022-02-14 22:27:41 -0300

    Create function: find environment PATH

--- src/main.c
+++ src/main.c
@@ -12,15 +12,29 @@
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+void	find_env_path(char **envp)
+{
+	char	**env_path;
+	int		i;
+
+	i = 0;
+	while (ft_strnstr(envp[i], "PATH", 4) == NULL)
+		i++;
+	env_path = ft_split(envp[i] + 5, ':');
+	ft_putstr_fd(env_path[0], STDOUT);
+	ft_putstr_fd("\n", STDOUT);
+}
+
+int	main(int argc, char **argv, char **envp)
 {
 	// int		pid;
 	// int		fd[2];
-	// char	**path;
+	// char	**env_path;
 
 	ft_putstr_fd(argv[0], STDOUT);
 	ft_putstr_fd("\n", STDOUT);
 	if (argc != 5)
 		error_handler("Bad arguments", "Ex: ./pipex file1 cmd1 cmd2 file2");
+	find_env_path(envp);
 	return (EXIT_SUCCESS);
 }
